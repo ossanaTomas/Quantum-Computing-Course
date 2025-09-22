a3914ddfae61cecc9ca5e942000891a26b24de1d
ossanaTomas <ossanatomas5@gmail.com>
2025-09-22 15:01:51 -0300

    primeros ejercicios

---
+++ src/Capitulo1.py
@@ -0,0 +1,73 @@
+import numpy as np
+import cmath as c
+import math 
+
+def suma(a,b): 
+    return a+b
+
+def div(a,b):
+    return a/b
+
+a= suma(1,8)
+b=div(1+3j,5+8j)
+print(b)
+print(c.polar(a))
+print(c.phase(a)) 
+
+print(dir(c))
+print(c.rect(1,0))
+
+
+#Programming Drill 2.1.1
+#Write three functions that perform the addition, inverse,
+#and scalar multiplication operations for Cn, i.e., write a function that accepts the ap
+#propriate input for each of the operations and outputs the vector
+
+# When n=1, the matrices Cm×n = Cm×1 = Cm,Thus, we can think of vectors as special types of matrices.
+def matrix_sum(v,w): 
+    if(len(v)==len(w)):
+        return v+w
+    else:
+        return print("diferent vector len")
+
+def matrix_inverse(v):
+    return v*(-1)
+
+def matrix_escalar_multiplication(escalar,matrix):
+    return escalar*matrix
+
+a= np.array([(1+2j,4+5j, 2+1j),(4-3j,9+2j,1+3j),(1,1,1)])
+b=np.array([(1,0,0),(0,1,0),(0,0, 1)])
+
+print(matrix_sum(a,b))
+print(matrix_inverse(b))
+print(matrix_escalar_multiplication(3,b) )
+
+
+print(a.shape)
+print(b.shape)
+
+# Programming Drill 2.2.2 Write a function that accepts two complex matrices of the
+# appropriate size. The function should do matrix multiplication and return the result
+
+def matrix_multiplication(Ma_A, Ma_B): 
+    (n,m)=Ma_A.shape
+    (p,k)=Ma_B.shape
+    if(m!=p):
+        return "error en los tamaños de las matrices"
+    Mult=np.empty((n,k), dtype=Ma_A.dtype) #creo un elemento de numpy vacio de tamaño (n,k)
+    for fila_a in range(n):  #lo ultimo que vario es la fila de A , 
+        for columna_b in range(k):   
+            valor=0          #reinicio el valor de los elementos puntuales 
+            for elemento in range(m):
+                valor+= Ma_A[fila_a,elemento]*Ma_B[elemento,columna_b]
+        Mult[fila_a,columna_b]=valor
+    return Mult
+    
+
+print(matrix_multiplication(a,b))
+
+
+#ProgrammingDrill2.4.1 Write a function that accepts two complex vectors of lengt n 
+# and calculates their inner product.
+
